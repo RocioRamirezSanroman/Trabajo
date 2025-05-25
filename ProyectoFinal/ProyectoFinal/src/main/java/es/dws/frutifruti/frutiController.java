02d0363bbcc42f57fc18c3028620f792029b5c19
Masterdark89 <126896860+Masterdark89@users.noreply.github.com>
2025-05-25 13:02:46 +0200

    Funcionalidad

--- ProyectoFinal/ProyectoFinal/src/main/java/es/dws/frutifruti/frutiController.java
+++ ProyectoFinal/ProyectoFinal/src/main/java/es/dws/frutifruti/frutiController.java
@@ -12,52 +12,25 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class frutiController {
-	
-	private List<Anuncio> anuncios = new ArrayList<>();
-	
-	public frutiController(){
-		anuncios.add(new Anuncio("mario", 0,"Fresas"));
-		anuncios.add(new Anuncio("antonio", 1,"Manzanas"));
-		anuncios.add(new Anuncio("cris", 2,"Piñas"));		
-	}
-	
-    @PostMapping("/guardarFormulario")
-    public String guardarAnuncio(@RequestParam String nombre,
-    							 @RequestParam int numero, 
-    							 @RequestParam String comentario) 
-    {	
-    	Anuncio anuncio = new Anuncio(nombre, numero, comentario);
-    	anuncios.add(anuncio);
-        return "volver";  
-    }
-    
-    @GetMapping("/pedidos")
-    public String irPedido(@RequestParam int numero, Model model) {
-        Anuncio anuncioEncontrado = anuncios.stream()
-            .filter(a -> a.getNumero() == numero)
-            .findFirst()
-            .orElse(null);
-        
-        if (anuncioEncontrado != null) {
-            model.addAttribute("anuncio", anuncioEncontrado);
-            return "pedido";
-        } else {
-            model.addAttribute("errorMessage", "No se encontró el pedido con número: " + numero);
-            return "error";
-        }
-    }
-    
-    @GetMapping("/")
-    public String mostrarAnuncios(Model model){
-    	model.addAttribute("anuncios", anuncios);
-    	
-		return "index";
-    }
-       
-    @GetMapping("/formulario")
-    public String irFormulario() {
-        return "formulario"; 
-    }
-    
-
+   
+	 @GetMapping("/index")
+	 public String irIndex() {
+		 return "index"; 
+	 }	
+		
+	 @GetMapping("/shop")
+	 public String irTienda() {
+	     return "shop"; 
+	 }
+	 
+	 @GetMapping("/cart")
+	 public String irCarrito() {
+	     return "cart"; 
+	 }
+	 
+	 @GetMapping("/chackout")
+	 public String irPago() {
+	     return "chackout"; 
+	 }
+	 
 }
