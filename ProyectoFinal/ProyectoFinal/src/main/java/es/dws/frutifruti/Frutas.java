e7170aecdd07b19d79030d3dca2e5f5cc6cd3c72
JaimeCastanedo <mordis0812@alumnos.alborfp.com>
2025-05-26 10:00:21 +0200

    BBDD2

--- ProyectoFinal/ProyectoFinal/src/main/java/es/dws/frutifruti/Frutas.java
+++ ProyectoFinal/ProyectoFinal/src/main/java/es/dws/frutifruti/Frutas.java
@@ -29,6 +29,15 @@ public class Frutas {
         this.cantidad = cantidad;
     }
 
+    List<Producto> productos = Arrays.asList(
+        new Frutas('Platanos', 2.99, 20),
+        new Frutas('Manzanas', 4.99, 20),
+        new Frutas('Frambuesas', 4.99, 20),
+        new Frutas('Albaricoques', 5.99, 20),
+        new Frutas('Uvas', 9.99, 20),
+        new Frutas('Naranjas', 3.99, 20)
+    );
+
     @OneToMany(mappedBy="frutas")
     private List<Carrito> carrito;
 }
--- ProyectoFinal/ProyectoFinal/src/main/java/es/dws/frutifruti/Verduras.java
+++ ProyectoFinal/ProyectoFinal/src/main/java/es/dws/frutifruti/Verduras.java
@@ -30,6 +30,15 @@ public class Verduras {
         this.cantidad = cantidad;
     }
 
+    List<Producto> productos = Arrays.asList(
+        new Verduras('Pepinos', 7.99, 20),
+        new Verduras('Patatas', 4.99, 20),
+        new Verduras('Pimientos', 6.99, 20),
+        new Verduras('Perejil', 1.99, 20),
+        new Verduras('Zanahorias', 5.99, 20),
+        new Verduras('Brocoli', 4.99, 20)
+    );
+
     @OneToMany(mappedBy="verduras")
     private List<Carrito> carrito;
 }
